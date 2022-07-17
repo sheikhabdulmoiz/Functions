f5da7b642b91b6f3736d03414ccedc8a1421ec3c
Abdul Moiz <abdulmoiz15000@gmail.com>
2022-07-17 16:53:35 +0500

    Functions

---
+++ app.js
@@ -0,0 +1,212 @@
+function l(a) {
+  console.log(a);
+}
+ 
+
+// function dateTime(){
+//     l(new Date());
+// }
+// dateTime();
+
+
+// function userGreet(firstName, lastName){
+//     l("Hello! "+firstName+" "+lastName)
+// }
+// userGreet("Abdul", "Moiz")
+
+
+// var num1=+prompt("Enter number1")
+// var num2=+prompt("Enter number1")
+// function sum(nm1,nm2){
+//     return nm1+nm2;
+// }
+// var S=sum(num1,num2);
+// l("Sum is: "+S);
+
+
+// function sum(num1,num2){
+//         return num1+num2;
+//     }
+// var S=sum(6,4)
+// l(S)
+
+
+// function sum(num1,num2){
+//          l(num1+num2);
+//     }
+// sum(6,4)
+
+
+// function calc(num1,num2,opt){
+//     if(opt==="+"){
+//         return num1+num2;
+//     }
+//     else if (opt==="-"){
+//         return num1-num2;
+//     }
+//     else if (opt==="*"){
+//         return num1*num2;
+//     }
+//     else if (opt==="/"){
+//         return num1/num2;
+//     }
+//     else if (opt==="%"){
+//         return num1%num2;
+//     }
+// }
+// var ans=calc(4,3,"%");
+// l("Answer is: "+ans)
+
+
+// function sqr(number){
+//     return number*number;
+// }
+// var S=sqr(3);
+// l("Square is: "+S)
+
+
+// function fact(num){
+//     factorial=1;
+// for(i=1;i<=num;i++){
+//      factorial=factorial*i;
+// }
+//     return factorial;
+// }
+// var ft=fact(6);
+// l(ft)
+
+
+// function counting(start,end){
+//     for(i=start;i<=end;i++){
+//         l(i);
+//     }
+// }
+// counting(3,20)
+
+
+// function hyp(base,perp){
+//     b1=base, p1=perp
+//     var base2,perpendicular2,final,last;
+//     square(b1,p1)
+//     function square(b1,p1){
+//         base2=b1*b1
+//         perpendicular2=p1*p1
+//     }
+//     last=base2+perpendicular2
+//     final=Math.sqrt(last)
+//     l("Hypotenuse is: "+final)
+// }
+// hyp(3,4)
+
+
+// function areaOfRectangle(w,h){
+//     var Area=w*h;
+//     l("Area of rectangle is: "+Area)
+// }
+// areaOfRectangle(3,4)
+// var W=6,H=6;
+// areaOfRectangle(W,H)
+
+
+// function palindrome(word) {
+//   var S = word.split("");
+// //   l(S);
+//   var R = S.reverse();
+// //   l(R);
+//   var J = R.join("");
+// //   l(J);
+//   if (J === word) {
+//     l("Given word is palindrome: " + word);
+//   } else {
+//     l("Given word isn't palindrome: " + word);
+//   }
+// }
+// palindrome("Abdul");
+// palindrome("madam");
+// palindrome("malayalam");
+
+
+// function firstLetterChange(line){
+// var S=line.split(" ")
+//       var charc;
+//       var rep1="";
+// for(var i=0;i<S.length;i++){
+//    charc=S[i] 
+//    var rep=charc.replace(charc[0],charc[0].toUpperCase())
+//        rep1+=rep+" "
+//   }
+//   l("Capitalize output: "+rep1)
+//  }
+// firstLetterChange("the quick brown fox");
+    
+
+// function longestStringFind(string){
+//   var arr=string.split(" ")
+//   // l(arr) 
+//     var largest=arr[0].length
+//     var new1;
+//   for(var i=0;i<arr.length;i++){
+//       if(largest<arr[i].length){ 
+//           //  largest=arr[i].length
+//            new1=arr[i]
+//       }
+//   }
+//   // l(largest)
+//   l(new1)
+// }
+// longestStringFind("Web Development Tutorial")
+
+
+// function occurence(string,char){
+//   newString=string.toLowerCase()
+//   var count=0;
+//   for(var i=0;i<newString.length;i++){
+//   if(newString[i]===char){
+//         count=count+1;
+//     }
+//   }
+//   l("Number of Occurence of "+"'"+char+"'"+" is "+count+".")
+// }
+// occurence('JSResourceS.com', 'o')
+
+
+// function calcCircumference(radius){
+//       var circumference=2*3.142*radius
+//       l("The circumference of circle is "+circumference)
+// }
+// calcCircumference(2)
+// calcCircumference(3)
+
+// function calcArea(radius){
+//       var area=3.142*radius*radius
+//       l("The Area of Circle is "+area)
+// }
+// calcArea(2)
+// calcArea(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// House Number 29/2, Liaquat Avenue Model Colony, Karachi east
+// ہاؤس نمبر ٢٩/٢  لیاقت ایونیو ماڈل کالونی
+//  مکان  نمبر 29/2  ،محلہ  لیاقت ایونیو ماڈل کالونی،کراچیرقی
+// مکان  نمبر 29/2  ،محلہ  لیاقت ایونیو ماڈل کالونی،کراچی شرقی
+// AllahMadad00!
